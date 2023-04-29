bb6a99b1fec05f6079a1d798daf7fbfc0b3b121b
Firdavs-coder <shodiyev-f@mail.ru>
2023-04-29 23:53:06 +0500

    Updated

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 from fastapi import FastAPI
+from typing import List
+from models import Gender, Role, User
+from uuid import uuid4
 
 app = FastAPI()
 
+db: List[User] = [
+    User(
+        id=uuid4(),
+        first_name="Jamila",
+        last_name="Ahmed",
+        gender=Gender.female,
+        roles=[Role.student],
+    ),
+    User(
+        id=uuid4(),
+        first_name="Alex",
+        last_name="Jones",
+        gender=Gender.female,
+        roles=[Role.admin, Role.user],
+    )
+]
+
 
 @app.get("/")
-def root():
+async def root():
     return {"username": "CoderProTeam"}
+
+
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db
+
+
+@app.post("/api/v1/users")
+async def register_user(user: User):
+    db.append(user)
+    return {"id": user.id}
